29ec5828f508279c83eae7c186b6777f2891a83c
Mikhail_Golovkov <mik270400@mail.ru>
2022-12-11 12:34:42 +0300

    ASF

--- dva/Program.cs
+++ dva/Program.cs
@@ -1,10 +1,7 @@
-﻿Clearndgsrtedf
-#pragma warning disable format
-fDS
-f
+﻿saFdsfdsf
+dsfsd
+fdgfhgfjgf
+dgdfgfdsfdsf
 
-#pragma warning disable format
-sd
 
-#pragma warning restore format
-#pragma warning restore format
+dsfsd
